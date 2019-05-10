e3e269e33d28adc570425ce221afade7750be3f0
Mary Gomez <Mary@Marys-MacBook-Pro.local>
2019-05-09 23:22:25 -0700

    Using typescript to build the app

--- app.ts
+++ app.ts
@@ -1,56 +1,64 @@
-"use strict";
+
 /**
  * To run this app execute:
  * npm start
- *
+ * 
  * This will load the package.json and execute what is there
  */
-exports.__esModule = true;
 
-var electron_1 = require("electron");
+// const { app, BrowserWindow } = require('electron')
+import { app, BrowserWindow } from 'electron'
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var win = null;
+let win : BrowserWindow = null
+
 function createWindow() {
     // Create the browser window.
-    win = new electron_1.BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             nodeIntegration: true
         }
-    });
+    })
+
     // and load the index.html of the app.
-    win.loadFile('index.html');
+    win.loadFile('index.html')
+
     // Open the DevTools.
-    win.webContents.openDevTools();
+    win.webContents.openDevTools()
+
     // Emitted when the window is closed.
-    win.on('closed', function () {
+    win.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null;
-    });
+        win = null
+    })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-electron_1.app.on('ready', createWindow);
+app.on('ready', createWindow)
+
 // Quit when all windows are closed.
-electron_1.app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        electron_1.app.quit();
+        app.quit()
     }
-});
-electron_1.app.on('activate', function () {
+})
+
+app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
-        createWindow();
+        createWindow()
     }
-});
+})
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
